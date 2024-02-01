3d2c71cbf5892a173df5d728de10ea1f732cdc8c
SAVG <1992savg@gmail.com>
2024-01-31 21:11:41 -0300

    Update root.tsx and _index.tsx files

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,49 @@
 import type { MetaFunction } from "@remix-run/node";
+import { ModeToggle } from "~/components/mode-toggle";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "Pokedex" },
+    { name: "Pokedex app", content: "Welcome to your Pokedex!" },
   ];
 };
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+		<div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.8' }} className="font-sans line-height-1.8">
+			<h1
+      className="text-4xl font-bold text-center mt-20 mb-10"
+      >Welcome to Remix</h1>
+			<ul>
+				<li>
+					<a
+						target='_blank'
+						href='https://remix.run/tutorials/blog'
+						rel='noreferrer'
+					>
+						15m Quickstart Blog Tutorial
+					</a>
+				</li>
+				<li>
+					<a
+						target='_blank'
+						href='https://remix.run/tutorials/jokes'
+						rel='noreferrer'
+					>
+						Deep Dive Jokes App Tutorial
+					</a>
+				</li>
+				<li>
+					<a
+						target='_blank'
+						href='https://remix.run/docs'
+						rel='noreferrer'
+					>
+						Remix Docs
+					</a>
+				</li>
+			</ul>
+			<ModeToggle />
+		</div>
+	);
 }
