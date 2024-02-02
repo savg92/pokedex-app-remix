b2140495045cb5f7c973ff35a19ef2b38da056ac
SAVG <1992savg@gmail.com>
2024-02-01 23:13:58 -0300

    Update mode toggle styling

--- app/components/Layout/Header/mode-toggle.tsx
+++ app/components/Layout/Header/mode-toggle.tsx
@@ -21,11 +21,11 @@ export function ModeToggle() {
 					size='icon'
 				>
 					<Sun className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
-					<Moon className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
+					<Moon className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 mt-[-1rem]' />
 					<span className='sr-only'>Toggle theme</span>
 				</Button>
 			</DropdownMenuTrigger>
-			<DropdownMenuContent align='end'>
+			<DropdownMenuContent align='end' className='bg-gray-500 dark:bg-gray-800 w-20'>
 				<DropdownMenuItem onClick={() => setTheme(Theme.LIGHT)}>
 					Light
 				</DropdownMenuItem>
