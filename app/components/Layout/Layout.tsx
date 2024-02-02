db72bfe767ef627bde53139acbd9e5cc2fcf0a37
SAVG <1992savg@gmail.com>
2024-02-01 23:15:03 -0300

    Update text color in main section

--- app/components/Layout/Layout.tsx
+++ app/components/Layout/Layout.tsx
@@ -7,7 +7,7 @@ const Layout = () => {
 	return (
 		<div className='flex flex-col min-h-screen justify-between'>
 			<Header />
-			<main className='flex-1 flex flex-col justify-center items-center bg-gray-100 dark:bg-gray-800 dark:text-white'>
+			<main className='flex-1 flex flex-col justify-center items-center bg-gray-100 dark:bg-gray-800 text-black dark:text-white'>
 				<Outlet />
 			</main>
 			<Footer />
