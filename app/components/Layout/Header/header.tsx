f35293e5dfeffaa2b163aad34b611317fdf6e462
SAVG <1992savg@gmail.com>
2024-02-05 20:05:39 -0300

    Refactor header component and update header links

--- app/components/Layout/Header/header.tsx
+++ app/components/Layout/Header/header.tsx
@@ -1,48 +1,48 @@
-import { Link } from "@remix-run/react"
-import { ModeToggle } from "./mode-toggle"
+import { Link } from '@remix-run/react';
+import { ModeToggle } from './mode-toggle';
 import Logo from '../../../assets/pokemon.svg';
 
 const headerLinks = [
-    {
-        name: "Home",
-        path: "/",
-    },
-    {
-        name: "About",
-        path: "/about",
-    },
-    {
-        name: "Services",
-        path: "/services",
-    },
-    {
-        name: "Contact",
-        path: "/contact",
-    },
-    ]
+	{
+		name: 'Home',
+		path: '/',
+	},
+	{
+		name: 'Favorites',
+		path: '/favorites',
+	},
+];
 
 const Header = () => {
-  return (
-    <>
-        <header className="flex justify-between items-center p-4">
-            <Link 
-                to="/"
-            className="flex items-center gap-2">
-                <img src={Logo} alt="Logo" className="w-20"/>
-            </Link>
-            <nav>
-                <ul className="flex gap-4 text-lg font-semibold uppercase tracking-wider" role="navigation">
-                    {headerLinks.map((link) => (
-                        <li key={link.path}>
-                            <Link to={link.path}>{link.name}</Link>
-                        </li>
-                    ))}
-                </ul>
-            </nav>
-            <ModeToggle />
-        </header>
-    </>
-  )
-}
+	return (
+		<>
+			<header className='flex justify-between items-center p-4'>
+				<Link
+					to='/'
+					className='flex items-center gap-2'
+				>
+					<img
+						src={Logo}
+						alt='Logo'
+						className='w-20'
+					/>
+				</Link>
+				<nav>
+					<ul
+						className='flex gap-4 text-lg font-semibold uppercase tracking-wider'
+						role='navigation'
+					>
+						{headerLinks.map((link) => (
+							<li key={link.path}>
+								<Link to={link.path}>{link.name}</Link>
+							</li>
+						))}
+					</ul>
+				</nav>
+				<ModeToggle />
+			</header>
+		</>
+	);
+};
 
-export default Header
+export default Header;
